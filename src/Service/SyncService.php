e213c1665c8c8ab8600a8d36dbcd251143fdf47a
Ilya Beliaev <info@php-dev.info>
2020-08-08 10:46:18 +0200

    initial commit.

--- src/Service/SyncService.php
+++ src/Service/SyncService.php
@@ -2,9 +2,33 @@
 
 namespace N3XT0R\MysqlSync;
 
+use Collective\Remote\RemoteManager;
+
 class SyncService
 {
-    public function __construct()
+
+    protected $sshManager;
+
+
+    public function __construct(RemoteManager $sshManager)
     {
+        $this->setSshManager($sshManager);
     }
+
+    /**
+     * @return RemoteManager
+     */
+    public function getSshManager(): RemoteManager
+    {
+        return $this->sshManager;
+    }
+
+    /**
+     * @param mixed $sshManager
+     */
+    public function setSshManager(RemoteManager $sshManager): void
+    {
+        $this->sshManager = $sshManager;
+    }
+
 }
